198963876d67393665e4cb4e5c95b7d2cdab25a8
Eric Dahl <eric.c.dahl@gmail.com>
2014-10-18 17:49:14 -0500

    add 37

---
+++ p037.py
@@ -0,0 +1,19 @@
+import p007
+
+primes = p007.primes(1000000)
+
+def truncatable(n):
+    sn = str(n)
+    if len(sn) < 2:
+        return False
+
+    for i in range(1, len(sn)):
+        if int(sn[0:i]) not in primes or int(sn[-i:]) not in primes:
+            return False 
+    return True
+
+tprimes = filter(truncatable, primes)
+
+print tprimes
+print len(tprimes)
+print sum(tprimes)
