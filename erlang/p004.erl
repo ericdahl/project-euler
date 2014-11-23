6ada24eb0e3c6ffa2ebaae23eab971387e12ae21
Eric Dahl <eric.c.dahl@gmail.com>
2014-11-22 20:28:59 -0800

    add 4 (erlang)

---
+++ erlang/p004.erl
@@ -0,0 +1,24 @@
+-module(p004).
+
+-export([run/0]).
+
+p(N) ->
+  L = integer_to_list(N),
+  L == lists:reverse(L).
+
+f() -> f(999, 999, 1).
+f(1, 1, Max) -> Max;
+f(1, J, Max) -> f(999, J - 1, Max);
+f(I, J, Max) ->
+  Pal = p(I * J),
+  if
+    not Pal -> f(I - 1, J, Max);
+    true ->
+      if
+        I * J > Max -> f(I - 1, J, I * J);
+        true -> f(I - 1, J, Max)
+      end
+  end.
+
+run() ->
+  io:format("~p ~n", [f()]).
