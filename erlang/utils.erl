2cb65723162ccbfc0dcb835ee80b23f0cee71887
Eric Dahl <eric.c.dahl@gmail.com>
2014-11-30 20:13:16 -0800

    erlang: add divisors function to utils

--- erlang/utils.erl
+++ erlang/utils.erl
@@ -1,6 +1,7 @@
 -module(utils).
 
 -export([primes/1]).
+-export([divisors/1]).
 
 primes(N) ->
   L = lists:seq(2, N),
@@ -12,3 +13,6 @@ primes(L, I, Max) when I > Max ->
 primes(L, I, Max) ->
   primes(lists:filter(fun(J) -> J =< I orelse J rem I /= 0 end, L), I + 1, Max).
 
+
+divisors(N) ->
+  [ A || A <- lists:seq(2, N div 2), N rem A == 0].
