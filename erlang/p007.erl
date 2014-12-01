dbf9285db6edf690f1ba0aa86f36bc9936d01107
Eric Dahl <eric.c.dahl@gmail.com>
2014-11-30 19:32:13 -0800

    erlang: refactor 7 to use primes module

--- erlang/p007.erl
+++ erlang/p007.erl
@@ -2,15 +2,8 @@
 
 -export([run/0]).
 
-p(N) ->
-  L = lists:seq(2, N),
-  p(L, 2, math:sqrt(N)).
-
-p(L, I, Max) when I > Max ->
-  L;
-
-p(L, I, Max) ->
-  p(lists:filter(fun(J) -> J =< I orelse J rem I /= 0 end, L), I + 1, Max).
+f() ->
+  lists:nth(10001, primes:primes(1000000)).
 
 run() ->
-  io:format("~p ~n", [lists:nth(10001, p(1000000))]).
+  io:format("~p ~n", [f()]).
