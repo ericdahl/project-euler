c0c7ce8f80f25088b1596c04abb89c1b8d2d08c7
Eric Dahl <eric.c.dahl@gmail.com>
2014-12-06 19:37:28 -0800

    erlang: parallelize 21

--- erlang/p021.erl
+++ erlang/p021.erl
@@ -1,12 +1,21 @@
 -module(p021).
 
--export([start/0]).
+-export([start/0, f/2]).
 
 d(N) ->
   lists:sum(utils:divisors(N)).
 
-f() ->
-  lists:sum([ X || X <- lists:seq(1, 10000), X /= d(X), X == d(d(X))]).
+f(I, J) ->
+  lists:sum([ X || X <- lists:seq(I, J), X /= d(X), X == d(d(X))]).
+
+launch(Processes) ->
+  lists:sum([ rpc:yield(P) || P <- launch(1, 10000 div Processes, 10000) ]).
+
+launch(I, _, Max) when I > Max ->
+  [];
+
+launch(I, Increment, Max) ->
+  launch(I + Increment, Increment, Max) ++ [rpc:async_call(node(), ?MODULE, f, [I, I + Increment - 1])].
 
 start() ->
-  io:format("~p ~n", [f()]).
+  io:format("~p ~n", [launch(8 * erlang:system_info(logical_processors_available))]).
