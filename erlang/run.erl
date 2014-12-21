df7e036fcf06274c71593ba0e208e0ac1405d4e4
Eric Dahl <eric.c.dahl@gmail.com>
2014-12-20 19:10:05 -0600

    erlang: basic Makefile

--- erlang/run.erl
+++ erlang/run.erl
@@ -1,6 +1,7 @@
 #!/usr/bin/env escript
 
 main([String]) ->
+  true = code:add_pathz("ebin/"),
   M = element(2, code:load_abs("ebin/" ++ String)),
   TimeMillis = element(1, timer:tc(M, start, [])) div 1000,
   io:format("~p  ~p ~n", [String, TimeMillis]),
