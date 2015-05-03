59fee3ca5151e5de670f28011789f70910a3547c
Eric Dahl <eric.c.dahl@gmail.com>
2015-05-03 09:53:45 -0700

    erlang: add 66

---
+++ erlang/src/p066.erl
@@ -0,0 +1,27 @@
+-module(p066).
+
+-export([start/0]).
+
+f() ->
+  element(2, lists:max([ {pell(D), D} || D <- lists:seq(2, 1000), not is_square(D) ])).
+
+pell(D) -> pell(D, 1, 1, 1, 0).
+
+pell(D, P, K, X1, Y) when K == 1 andalso Y /= 0 ->
+  X1;
+
+pell(D, P, K, X, Y) ->
+  Tmp = K * (P div K + 1) - P,
+  P_Next = Tmp - K * trunc((Tmp - math:sqrt(D)) / K),
+  X_Next = (P_Next * X + D * Y) div abs(K),
+  Y_Next = (P_Next * Y + X) div abs(K),
+  K_Next = (P_Next * P_Next - D) div K,
+  pell(D, P_Next, K_Next, X_Next, Y_Next).
+
+is_square(N) ->
+  Root = trunc(math:sqrt(N)),
+  Root * Root == N.
+
+start() ->
+  io:format("~p ~n", [f()]).
+
