cfae44ff09518cdb61cc1cf597fa2536a6013173
Eric Dahl <eric.c.dahl@gmail.com>
2015-06-22 08:40:38 -0700

    go: add 4

---
+++ go/p004.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "strconv"
+
+func main() {
+    max := 0
+    for i := 100; i < 1000; i++ {
+        for j := 100; j < 1000; j++ {
+            if i * j == reverse(i * j) {
+                if i * j > max {
+                    max = i * j
+                }
+            }
+        }
+    }
+    fmt.Println(max)
+}
+
+func reverse(n int) (int) {
+    result := 0
+    for ; n > 0 ; n /= 10 {
+        result *= 10
+        result += n % 10
+    }
+    return result
+}
+
