fe520d5c7bc1eb1622c65208cf1c04888c783669
Eric Dahl <eric.c.dahl@gmail.com>
2015-06-26 17:32:48 -0700

    go: add Primes util

--- go/utils.go
+++ go/utils.go
@@ -1,6 +1,5 @@
 package main
 
-
 func Factors(n uint64) ([]uint64) {
     factors := make([]uint64, 0, 2)
 
@@ -16,4 +15,25 @@ func Factors(n uint64) ([]uint64) {
     return factors
 }
 
+func Primes(n uint64) ([]uint64) {
+    composites := make([]bool, n)
+    composites[0] = true
+    composites[1] = true
 
+    for i := uint64(2); i < n; {
+        for j := i * 2; j < n; j += i {
+            composites[j] = true
+        }
+        for i++ ; i < n && composites[i]; i++ {}
+    }
+
+    primes := make([]uint64, 0)
+
+    for i := uint64(2); i < n; i++ {
+        if !composites[i] {
+            primes = append(primes, i)
+        }
+    }
+
+    return primes
+}
