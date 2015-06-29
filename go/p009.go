f27f2727c3dd924d6a39d2ec2239adce8ab5b258
Eric Dahl <eric.c.dahl@gmail.com>
2015-06-29 08:27:24 -0700

    go: add 9

---
+++ go/p009.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    for a := 1; a < 1000; a++ {
+        for b := a; b < 1000; b++ {
+            c := 1000 - a - b
+            if c * c == a * a + b * b {
+                fmt.Println(a * b * c)
+            }
+        }
+    }
+}
+
