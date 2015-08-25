65aefb67caab60dd77e9438ff6bfa69cde005483
Eric Dahl <eric.c.dahl@gmail.com>
2015-08-24 18:45:36 -0700

    go: refactor 38 to use utils.Pandigital

--- go/src/p038/p038.go
+++ go/src/p038/p038.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "utils"
     "strconv"
 )
 
@@ -9,7 +10,7 @@ func main() {
     max := ""
     for i := 1; i < 10000; i++ {
         result := concat(i)
-        if result > max && pandigital(result) {
+        if result > max && utils.PandigitalString(result) {
             max = result
         }
     }
@@ -22,18 +23,4 @@ func concat(n int) (string) {
         result += strconv.Itoa(n * i)
     }
     return result
-}
-
-func pandigital(n string) (bool) {
-    if len(n) != 9 {
-        return false
-    }
-    m := make(map[rune]bool)
-    for _, c := range n {
-        if c == '0' || m[c] {
-            return false
-        }
-        m[c] = true
-    }
-    return true
 }
